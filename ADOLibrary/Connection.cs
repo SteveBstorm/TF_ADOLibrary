b4c574b497ea4c1750f7cc7868e8c43fb9b8ebe7
steveBstorm <steve.lorent@bstorm.be>
2021-09-13 13:18:52 +0200

    mise en place

---
+++ ADOLibrary/Connection.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ADOLibrary
+{
+    public class Connection
+    {
+        private readonly string _connectionString;
+
+        public Connection(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+
+        /*
+         ExecuteScalar() => return une valeur
+         ExecuteNonQuery() => return le nombre de row affectée
+         ExecuteReader() => return un jeu d'enregistrement / minimum 1 voir un tableau d'enregistrement
+
+         SqlConnection
+
+         SqlCommand
+         */
+
+        private SqlConnection createConnection()
+        {
+            SqlConnection connection = new SqlConnection(_connectionString);
+            return connection;
+        }
+
+        private SqlCommand createCommand(Command cmd, SqlConnection connection)
+        {
+            SqlCommand sqlCmd = connection.CreateCommand();
+            sqlCmd.CommandText = cmd._query;
+            sqlCmd.CommandType = cmd._isStoredProcedure ? System.Data.CommandType.StoredProcedure : System.Data.CommandType.Text;
+            if(!(cmd._parameters is null))
+            {
+                foreach (KeyValuePair<string, object> param in cmd._parameters)
+                {
+                    SqlParameter parameter = sqlCmd.CreateParameter();
+                    parameter.ParameterName = param.Key;
+                    parameter.Value = param.Value;
+
+                    sqlCmd.Parameters.Add(parameter);
+                }
+            }
+            return sqlCmd;
+        }
+    }
+}
